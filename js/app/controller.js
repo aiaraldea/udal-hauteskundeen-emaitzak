817602a3421d9fc76d8078a9d28bedb72c796bd0
gorbeia <inakiibarrola@gmail.com>
2019-05-12 22:09:47 +0200

    Sakelako telefonoetara egokitu

--- js/app/controller.js
+++ js/app/controller.js
@@ -35,11 +35,12 @@ aiaraldea.hauteskundeak2015.controller = function (_model) {
     udalak.init();
     urteak = aiaraldea.hauteskundeak2015.urteak(component, model, clickCallback);
 
-    bilakaera = aiaraldea.hauteskundeak2015.bilakaera(component, model);
-    pie1 = aiaraldea.hauteskundeak2015.pie(component, model, 'Erroldaren gaineko ehunekoa');
+    var grafikoak = $("<div id='grafikoak'>").appendTo(component);
+    bilakaera = aiaraldea.hauteskundeak2015.bilakaera(grafikoak, model);
+    pie1 = aiaraldea.hauteskundeak2015.pie(grafikoak, model, 'Erroldaren gaineko ehunekoa');
 //    pie2 = aiaraldea.hauteskundeak2015.pie(component, model, 'Zinegotziak');
 
-    pie2 = aiaraldea.hauteskundeak2015.d3pie.create(_component, 'adg-zinegotziak');
+    pie2 = aiaraldea.hauteskundeak2015.d3pie.create(grafikoak[0], 'adg-zinegotziak');
     table = aiaraldea.hauteskundeak2015.table(component, model);
 
     model.hautatuak.udala = 'Aiaraldea';
@@ -65,7 +66,7 @@ aiaraldea.hauteskundeak2015.controller = function (_model) {
       urteak.hautatu(model.hautatuak.urtea);
       table.init(model.hautatuak.udala, model.hautatuak.urtea);
       var data1 = model.getPiechartVotesByCouncilAndYear(model.hautatuak.udala, model.hautatuak.urtea);
-      var data2 = model.getPiechartCouncilersByCouncilAndYear(model.hautatuak.udala, model.hautatuak.urtea);
+      // var data2 = model.getPiechartCouncilersByCouncilAndYear(model.hautatuak.udala, model.hautatuak.urtea);
       var data2 = model.getD3PiechartCouncilersByCouncilAndYear(model.hautatuak.udala, model.hautatuak.urtea);
       pie1.init(data1);
       pie2.draw(data2.zinegotziak, 'Zinegotziak', data2.koloreak);
